66670cc725dc856aa3672c0b5cc9168c30f90e55
Andra Antariksa <andra.antariksa@gmail.com>
2020-06-13 22:16:59 +0700

    Reset the project with a minimal table

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -11,18 +11,28 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // TODO
-    QTableView* table = findChild<QTableView*>("tableView");
+    tableWidget.reset(findChild<QTableWidget*>("tableWidget"));
+    tableWidget->insertRow(tableWidget->rowCount());
+    tableWidget->insertColumn(tableWidget->columnCount());
+    tableWidget->setVerticalHeaderItem(tableWidget->rowCount() - 1, new QTableWidgetItem("Total"));
+    tableWidget->setHorizontalHeaderItem(tableWidget->columnCount() - 1, new QTableWidgetItem("Total"));
 
-    model.append({ "Test", BankStatement::Panggilan::Cukup, BankStatement::Kartu::Prabayar, BankStatement::Blok::Sedang, false });
-    model.append({ "Test", BankStatement::Panggilan::Cukup, BankStatement::Kartu::Prabayar, BankStatement::Blok::Sedang, false });
-    model.append({ "Test", BankStatement::Panggilan::Cukup, BankStatement::Kartu::Prabayar, BankStatement::Blok::Sedang, false });
-    model.append({ "Test", BankStatement::Panggilan::Cukup, BankStatement::Kartu::Prabayar, BankStatement::Blok::Sedang, false });
-    model.append({ "Test", BankStatement::Panggilan::Cukup, BankStatement::Kartu::Prabayar, BankStatement::Blok::Sedang, false });
-
-    auto delegate_combobox = new DelegateComboBox();
-    table->setItemDelegate(delegate_combobox);
-    table->setModel(&model);
+    tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    for (int x = 0; x < tableWidget->columnCount(); ++x)
+    {
+        for (int y = 0; y < tableWidget->rowCount(); ++y)
+        {
+            auto temp = new QTableWidgetItem;
+            temp->setData(Qt::DisplayRole, 0);
+            // If on bottom or right edge
+            if (x == tableWidget->columnCount() - 1 || y == tableWidget->rowCount() - 1)
+            {
+                temp->setFlags(Qt::NoItemFlags);
+            }
+            tableWidget->setItem(y, x, temp);
+        }
+    }
+    tableWidget->setEditTriggers(QAbstractItemView::AllEditTriggers);
 }
 
 MainWindow::~MainWindow()
@@ -34,4 +44,43 @@ MainWindow::~MainWindow()
 void MainWindow::on_actionClose_triggered()
 {
     QCoreApplication::quit();
+}
+
+void MainWindow::on_tableWidget_itemChanged(QTableWidgetItem *item)
+{
+    if (tableWidget->editTriggers() == QAbstractItemView::AllEditTriggers)
+    {
+        populateTotal();
+    }
+
+    // TODO
+    // Calculation
+}
+
+void MainWindow::populateTotal()
+{
+    int acc_y;
+    for (int x = 0; x < tableWidget->columnCount() - 1; ++x)
+    {
+        acc_y = 0;
+        for (int y = 0; y < tableWidget->rowCount() - 1; ++y)
+        {
+            acc_y += tableWidget->item(y, x)->text().toInt();
+        }
+
+        tableWidget->item(tableWidget->rowCount() - 1, x)->setData(Qt::DisplayRole, acc_y);
+    }
+
+    int acc_x;
+    for (int y = 0; y < tableWidget->rowCount() - 1; ++y)
+    {
+        acc_x = 0;
+
+        for (int x = 0; x < tableWidget->columnCount() - 1; ++x)
+        {
+            acc_x += tableWidget->item(y, x)->text().toInt();
+        }
+
+        tableWidget->item(y, tableWidget->columnCount() - 1)->setData(Qt::DisplayRole, acc_x);
+    }
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -3,142 +3,11 @@
 
 #include <QMainWindow>
 #include <QtWidgets>
+#include <memory>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
-
-// TODO
-// Exract class
-class BankStatement
-{
-    Q_GADGET
-public:
-    enum class Panggilan
-    {
-        Sedikit,
-        Sedang,
-        Cukup,
-        Banyak,
-    };
-    Q_ENUM(Panggilan)
-
-    enum class Kartu
-    {
-        Prabayar,
-        Pascabayar,
-    };
-    Q_ENUM(Kartu)
-
-    enum class Blok
-    {
-        Rendah,
-        Sedang,
-        Tinggi,
-    };
-    Q_ENUM(Blok)
-
-    QString name;
-    Panggilan panggilan;
-    Kartu kartu;
-    Blok blok;
-    bool bonus;
-};
-Q_DECLARE_METATYPE(BankStatement::Panggilan)
-Q_DECLARE_METATYPE(BankStatement::Kartu)
-Q_DECLARE_METATYPE(BankStatement::Blok)
-
-class BankStatementModel: public QAbstractTableModel
-{
-private:
-    QList<BankStatement> data_list;
-public:
-    int rowCount(const QModelIndex &) const override {
-        return data_list.count();
-    }
-
-    int columnCount(const QModelIndex &) const override
-    {
-        return 5;
-    }
-
-    QVariant data(const QModelIndex &index, int role) const override
-    {
-        if (role != Qt::DisplayRole && role != Qt::EditRole) return {};
-
-        const auto & bank_statement = data_list[index.row()];
-
-        switch (index.column()) {
-        case 0: return bank_statement.name;
-        case 1: return QVariant::fromValue(bank_statement.panggilan).toString();
-        case 2: return QVariant::fromValue(bank_statement.kartu).toString();
-        case 3: return QVariant::fromValue(bank_statement.blok).toString();
-        case 4: return bank_statement.bonus;
-        default: return {};
-        };
-    }
-
-    QVariant headerData(int section, Qt::Orientation orientation, int role) const override {
-        if (orientation != Qt::Horizontal || role != Qt::DisplayRole) return {};
-
-        switch (section) {
-        case 0: return "Name";
-        case 1: return "Panggilan";
-        case 2: return "Jenis Kartu";
-        case 3: return "Blok";
-        case 4: return "Bonus";
-        default: return {};
-        }
-    }
-
-    void append(const BankStatement & bank_statement) {
-        beginInsertRows({}, data_list.count(), data_list.count());
-
-        data_list.append(bank_statement);
-
-        endInsertRows();
-    }
-
-    Qt::ItemFlags flags(const QModelIndex &index) const override
-    {
-        if (!index.isValid())
-        {
-            return Qt::ItemIsEnabled;
-        }
-
-        return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
-    }
-
-    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override
-    {
-        if (index.isValid() && role == Qt::EditRole) {
-
-            switch (index.column()) {
-            case 0:
-                data_list[index.row()].name = value.toString();
-                break;
-            case 1:
-                data_list[index.row()].panggilan = (BankStatement::Panggilan)value.toInt();
-                break;
-            case 2:
-                data_list[index.row()].kartu = (BankStatement::Kartu)value.toInt();
-                break;
-            case 3:
-                data_list[index.row()].blok = (BankStatement::Blok)value.toInt();
-                break;
-            case 4:
-                data_list[index.row()].bonus = value.toBool();
-                break;
-            default: return false;
-            }
-
-            emit dataChanged(index, index);
-
-            return true;
-        }
-        return false;
-    }
-};
 
 class MainWindow : public QMainWindow
 {
@@ -150,10 +19,12 @@ public:
 
 private slots:
     void on_actionClose_triggered();
+    void on_tableWidget_itemChanged(QTableWidgetItem *item);
 
 private:
-    Ui::MainWindow *ui;
+    void populateTotal();
+    std::unique_ptr<QTableWidget> tableWidget;
 
-    BankStatementModel model;
+    Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
