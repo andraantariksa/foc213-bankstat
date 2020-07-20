d8ee5fafa633b9fc15052b9342eff68460427ab9
Andra Antariksa <andra.antariksa@gmail.com>
2020-07-20 20:45:08 +0700

    Initial commit for ImGui SFML template

---
+++ README.md
@@ -0,0 +1,22 @@
+# Conditional Probability
+
+## Prerequisites
+
+- CMake >= 3.10
+- C++17 supported compiler
+
+## Installation
+
+- Clone to local
+- Run CMake and build
+
+```
+mkdir build
+cd build
+cmake ..
+make
+```
+
+## License
+
+Licensed under [MIT License](LICENSE)
---
+++ src/application.h
@@ -0,0 +1,21 @@
+#ifndef _APPLICATION_HPP_
+#define _APPLICATION_HPP_
+
+#include <SFML/Graphics.hpp>
+
+class Application
+{
+private:
+	// Window
+	std::unique_ptr<sf::RenderWindow> window_main;
+public:
+	Application(int window_width, int window_height, const sf::String& title);
+	~Application();
+
+	// Render the layers
+	void updateInterface();
+	// Main loop
+	void dispatch();
+};
+
+#endif //_APPLICATION_HPP_
